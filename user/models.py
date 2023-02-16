bc69d4a8b3c344cf5e90320edd5facb96234deda
MatRos-sf <mateusz-rosenkranz@wp.pl>
2023-02-16 22:28:33 +0100

    Add model user.Profile

--- user/models.py
+++ user/models.py
@@ -1,8 +1,13 @@
 from django.db import models
 from django.contrib.auth.models import User
 
+from tier.models import ProfileTier
+
 class Profile(models.Model):
 
     user = models.OneToOneField(User, on_delete=models.CASCADE)
+    profile_tier = models.ForeignKey(ProfileTier, on_delete=models.CASCADE, blank=True, null=True)
 
 
+    def __str__(self):
+        return self.user.username
