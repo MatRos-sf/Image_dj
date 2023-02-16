531f79bc4f5670ebb093324f5db0f09caa933b49
MatRos-sf <mateusz-rosenkranz@wp.pl>
2023-02-16 23:30:59 +0100

    create image model and gallery model

--- image/models.py
+++ image/models.py
@@ -1,3 +1,56 @@
 from django.db import models
+from django.utils import timezone
 
-# Create your models here.
+from datetime import timedelta
+
+from user.models import Profile
+
+
+class Gallery(models.Model):
+    name = models.CharField(max_length=100, null=True)
+    author = models.ForeignKey(Profile, on_delete=models.CASCADE, null=True)
+    created = models.DateTimeField(auto_now_add=True)
+
+def get_upload_path(instance, filename):
+    return f"{instance.profile.pk}/{filename}"
+
+class Image(models.Model):
+
+    profile = models.ForeignKey(Profile, on_delete=models.CASCADE)
+    image = models.ImageField(upload_to=get_upload_path)
+    expiry = models.PositiveIntegerField(null=True)
+
+    is_original = models.BooleanField(default=True)
+    is_active = models.BooleanField(default=True)
+    created = models.DateTimeField(auto_now_add=True)
+
+    gallery = models.ForeignKey(Gallery, on_delete=models.CASCADE, null=True, related_name='images')
+
+    @property
+    def get_profile_name(self):
+        return self.profile.user.username
+
+    @property
+    def get_profile_tier(self):
+        return self.profile.profile_tier.name
+
+    @property
+    def binary_permission(self):
+        return bool(self.profile.profile_tier.binary_file)
+
+    @property
+    def link_active(self):
+        if not self.expiry:     return True
+        date_now = timezone.now()
+        date_expiry = self.created + timedelta(seconds= self.expiry)
+        if self.is_active and date_expiry < date_now:
+            self.is_active = False
+            self.expiry = None
+            self.save()
+        return bool(date_expiry > date_now)
+
+
+    def __str__(self):
+        return f"{self.get_profile_name}__{self.get_profile_tier}: {self.id}"
+
+
