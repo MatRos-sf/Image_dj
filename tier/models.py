993b9f56f3b254112f20d3d636167bd936d539e7
MatRos-sf <mateusz-rosenkranz@wp.pl>
2023-02-16 20:51:50 +0100

    create tier.ProfileTier and tier.ThumbnailsSize

--- tier/models.py
+++ tier/models.py
@@ -1,3 +1,18 @@
 from django.db import models
 
-# Create your models here.
+class ThumbnailSize(models.Model):
+
+    size = models.PositiveIntegerField(unique=True)
+
+    def __str__(self):
+        return str(self.size)
+
+class ProfileTier(models.Model):
+
+    name = models.CharField(max_length=100)
+    thumbnail_sizes = models.ManyToManyField(ThumbnailSize)
+    original_file = models.BooleanField()
+    binary_file = models.BooleanField()
+
+    def __str__(self):
+        return self.name
